a13c93c6d110cafc221936bae6e66cd645aee983
Travis Foutz <tfoutz@byui.edu>
2020-09-29 14:26:39 -0700

    added stretch goal 2

--- web/w03TeamActivity.php
+++ web/w03TeamActivity.php
@@ -7,15 +7,15 @@
     </head>
     <body>
         <h1>Week 03 Team Activity</h1><br>
-        <!-- HTML form version -->
 
-        <!-- <form action="w03results.php" method="post">
+        <form action="w03results.php" method="post">
             <label for="name"> Name:</label>
             <input type="text" id="name" name="name"><br><br>
             <label for="email"> Email:</label>
             <input type="text" id="email" name="email"><br><br>
             
-            <p>Please select your Major:</p>
+            <!-- HTML version -->
+            <!-- <p>Please select your Major:</p>
             <input type="radio" id="cs" name="major" value="Computer Science">
             <label for="cs">Computer Science</label><br>
             <input type="radio" id="wdd" name="major" value="Web Design and Development">
@@ -23,56 +23,34 @@
             <input type="radio" id="cit" name="major" value="Computer information Technology">
             <label for="cit">Computer information Technology</label><br>
             <input type="radio" id="ce" name="major" value="Computer Engineering">
-            <label for="ce">Computer Engineering</label><br><br>
+            <label for="ce">Computer Engineering</label><br><br> -->
             
+            <!-- PHP version -->
+            <?php
+                $majors = array('Computer Science', 'Web Design and Developement', 'Computer Information Technology', 'Computer Engineering');
+                $ids = array('cs', 'wdd', 'cit', 'ce');
+                
+                for ($i = 0; $i < 4; $i++) {
+                    echo '<input type="radio" name="major" id="'.$ids[$i].'" value="'.$majors[$i].'">
+                        <label for="'.$ids[$i].'">'.$majors[$i].'</label><br>';
+                }
+                echo '<br>';
+            ?>
+
             <p>Continents you've visited:</p>
-			<input type="checkbox" name="continents[]" id="place-na" value="North America"><label for="place-na">North America</label><br />
-			<input type="checkbox" name="continents[]" id="place-sa" value="South America"><label for="place-sa">South America</label><br />
-			<input type="checkbox" name="continents[]" id="place-asia" value="Asia"><label for="place-asia">Asia</label><br />
-			<input type="checkbox" name="continents[]" id="place-eu" value="Europe"><label for="place-eu">Europe</label><br />
-			<input type="checkbox" name="continents[]" id="place-af" value="Africa"><label for="place-af">Africa</label><br />
-			<input type="checkbox" name="continents[]" id="place-aus" value="Australia"><label for="place-aus">Australia</label><br />
-			<input type="checkbox" name="continents[]" id="place-ant" value="Antarctica"><label for="place-ant">Antarctica</label><br />
+			<input type="checkbox" name="continents[]" id="place-na" value="na"><label for="place-na">North America</label><br />
+			<input type="checkbox" name="continents[]" id="place-sa" value="sa"><label for="place-sa">South America</label><br />
+			<input type="checkbox" name="continents[]" id="place-asia" value="asia"><label for="place-asia">Asia</label><br />
+			<input type="checkbox" name="continents[]" id="place-eu" value="eu"><label for="place-eu">Europe</label><br />
+			<input type="checkbox" name="continents[]" id="place-af" value="af"><label for="place-af">Africa</label><br />
+			<input type="checkbox" name="continents[]" id="place-aus" value="aus"><label for="place-aus">Australia</label><br />
+			<input type="checkbox" name="continents[]" id="place-ant" value="ant"><label for="place-ant">Antarctica</label><br />
             <br />
             
             <label for="comments"> Comments:</label><br>
             <textarea id="comments" name="comments" placeholder="Write something.." style="height:200px;width: 98%;"></textarea><br>
             
             <input type="submit">
-        </form> -->
-
-        <!-- PHP form version -->
-        <?php
-            echo '<form action="w03results.php" method="post">
-            <label for="name"> Name:</label>
-            <input type="text" id="name" name="name"><br><br>
-            <label for="email"> Email:</label>
-            <input type="text" id="email" name="email"><br><br>';
-
-            $majors = array('Computer Science', 'Web Design and Developement', 'Computer Information Technology', 'Computer Engineering');
-            $ids = array('cs', 'wdd', 'cit', 'ce');
-            
-            for ($i = 0; $i < 4; $i++) {
-                echo '<input type="radio" name="major" id="'.$ids[$i].'" value="'.$majors[$i].'">
-                    <label for="'.$ids[$i].'">'.$majors[$i].'</label><br>';
-            }
-            echo '<br>';
-            
-            echo '<p>Continents you have visited:</p>
-                <input type="checkbox" name="continents[]" id="place-na" value="North America"><label for="place-na">North America</label><br />
-                <input type="checkbox" name="continents[]" id="place-sa" value="South America"><label for="place-sa">South America</label><br />
-                <input type="checkbox" name="continents[]" id="place-asia" value="Asia"><label for="place-asia">Asia</label><br />
-                <input type="checkbox" name="continents[]" id="place-eu" value="Europe"><label for="place-eu">Europe</label><br />
-                <input type="checkbox" name="continents[]" id="place-af" value="Africa"><label for="place-af">Africa</label><br />
-                <input type="checkbox" name="continents[]" id="place-aus" value="Australia"><label for="place-aus">Australia</label><br />
-                <input type="checkbox" name="continents[]" id="place-ant" value="Antarctica"><label for="place-ant">Antarctica</label><br />
-                <br />';
-
-            echo '<label for="comments"> Comments:</label><br>
-                <textarea id="comments" name="comments" placeholder="Write something.." style="height:200px;width: 98%;"></textarea><br>';
-
-            echo '<button type="submit">Submit</button>';
-            echo '</form>';
-        ?>
+        </form>
     </body>
 </html>
