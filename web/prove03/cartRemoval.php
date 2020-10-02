80ea5ca75f08b7674cf2009675865207a2161f75
Travis Foutz <tfoutz@byui.edu>
2020-10-02 14:50:01 -0700

    adding/testing progress/debugging

--- web/prove03/cartRemoval.php
+++ web/prove03/cartRemoval.php
@@ -1,20 +1,20 @@
 <?php
     if(isset($_POST['button1'])) {
-        $_SESSION["cartItem1"] = "";
+        unset($_SESSION["cartItem1"]);
     }
     if(isset($_POST['button2'])) { 
-        $_SESSION["cartItem2"] = "";       
+        unset($_SESSION["cartItem2"]);
     }
     if(isset($_POST['button3'])) { 
-        $_SESSION["cartItem3"] = "";          
+        unset($_SESSION["cartItem3"]);
     } 
     if(isset($_POST['button4'])) { 
-        $_SESSION["cartItem4"] = "";         
+        unset($_SESSION["cartItem4"]);
     } 
     if(isset($_POST['button5'])) { 
-        $_SESSION["cartItem5"] = "";          
+        unset($_SESSION["cartItem5"]);
     } 
     if(isset($_POST['button6'])) { 
-        $_SESSION["cartItem6"] = "";          
+        unset($_SESSION["cartItem6"]);
     } 
 ?>
