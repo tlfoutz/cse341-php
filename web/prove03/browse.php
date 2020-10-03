906d6026c585b48d67b8bfac46b222f23fc40aa8
Travis Foutz <tfoutz@byui.edu>
2020-10-03 16:54:04 -0700

    adding/testing progress/debugging

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -25,40 +25,35 @@ session_start();
                     <td>Item #1</td>
                     <td>The first item</td>
                     <td><input type="number" name="num1" min="0" value="0">
-                    <td><input type="submit" name="button1" value="Add to cart"/>
                 </tr>
                 <tr>
                     <td>Item #2</td>
                     <td>The second item</td>
                     <td><input type="number" name="num2" min="0" value="0">
-                    <td><input type="submit" name="button2" value="Add to cart"/>
                 </tr>
                 <tr>
                     <td>Item #3</td>
                     <td>The third item</td>
                     <td><input type="number" name="num3" min="0" value="0">
-                    <td><input type="submit" name="button3" value="Add to cart"/>
                 </tr>
                 <tr>
                     <td>Item #4</td>
                     <td>The fourth item</td>
                     <td><input type="number" name="num4" min="0" value="0">
-                    <td><input type="submit" name="button4" value="Add to cart"/>
                 </tr>
                 <tr>
                     <td>Item #5</td>
                     <td>The fifth item</td>
                     <td><input type="number" name="num5" min="0" value="0">
-                    <td><input type="submit" name="button5" value="Add to cart"/>
                 </tr>
                 <tr>
                     <td>Item #6</td>
                     <td>The sixth item</td>
                     <td><input type="number" name="num6" min="0" value="0">
-                    <td><input type="submit" name="button6" value="Add to cart"/>
                 </tr>
             </table>
         </form>
+        <td><input type="submit" name="addToCart" value="Add to cart"/>
         <form action="cart.php">
             <input type="submit" value="Go to cart"/>
         </form>    
