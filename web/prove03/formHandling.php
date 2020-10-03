4008ff9eea683fe3a4abf49b4754310b2e3fdd06
Travis Foutz <tfoutz@byui.edu>
2020-10-03 16:39:45 -0700

    adding/testing progress/debugging

--- web/prove03/formHandling.php
+++ web/prove03/formHandling.php
@@ -3,53 +3,65 @@ $nameErr = $emailErr = $addressErr = $cityErr = $stateErr = $zipErr = "";
 $fullname = $email = $address = $city = $state = $zip = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $valid = true;
+
   if (empty($_POST["fullname"])) {
+    $valid = false;
     $nameErr = "Name is required";
   } else {
     $fullname = test_input($_POST["fullname"]);
     if (!preg_match("/^[a-zA-Z-' ]*$/",$fullname)) {
+      $valid = false;
       $nameErr = "Only letters and white space allowed";
     }
   }
   
   if (empty($_POST["email"])) {
+    $valid = false;
     $emailErr = "Email is required";
   } else {
     $email = test_input($_POST["email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $valid = false;
       $emailErr = "Invalid email format";
     }
   }
 
   if (empty($_POST["address"])) {
+    $valid = false;
     $addressErr = "Address is required";
   } else {
     $address = test_input($_POST["address"]);
   }
 
   if (empty($_POST["city"])) {
+    $valid = false;
     $cityErr = "City is required";
   } else {
     $city = test_input($_POST["city"]);
   }
 
   if (empty($_POST["state"])) {
+    $valid = false;
     $cityErr = "State is required";
   } else {
     $city = test_input($_POST["state"]);
   }
 
   if (empty($_POST["zip"])) {
+    $valid = false;
     $nameErr = "Zipcode is required";
   } else {
     $name = test_input($_POST["zip"]);
-    if (!preg_match("/^[0-9]{5}?$/", $zip)) {
+    if (!preg_match("/^[0-9]{5}*$/", $zip)) {
+      $valid = false;
       $zipErr = "Exactly 5 digits allowed";
     }
   }
 }
 
-if($valid){
+if($valid) {
   header('location:confirmation.php');
   exit();
 }
