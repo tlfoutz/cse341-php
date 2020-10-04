33317f545476a221b652f7bfaaee689293cb7216
Travis Foutz <tfoutz@byui.edu>
2020-10-03 19:27:08 -0700

    adding/testing progress/debugging

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -8,7 +8,6 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Travis Foutz">
         <meta name="description" content="Shopping Cart Assignment 03 - CSE 341">
-        <link rel="icon" type="image/png" href="images/moon.gif">
         <title>Cart</title>
     </head>
     <body>
