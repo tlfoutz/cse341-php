24fc5fe8611e631197f55ea12c1ad916965c60c7
Travis Foutz <tfoutz@byui.edu>
2020-10-09 17:04:27 -0700

    adding mySQLdb.sql

---
+++ db/mySQLdb.sql
@@ -0,0 +1,80 @@
+-- table of users
+-- locations and foods will be tied to users
+CREATE TABLE users (
+    id                  SERIAL          PRIMARY KEY    NOT NULL,
+    user_name           VARCHAR(16)                    NOT NULL,
+    user_password       VARCHAR(16)                    NOT NULL,
+    UNIQUE(user_name)
+);
+
+-- pre-seeded table of foodtypes
+-- either 'standalone', 'ingredient', or 'condiment'
+CREATE TABLE foodtypes (
+    id	        SERIAL		PRIMARY KEY     NOT NULL,
+    type_name   VARCHAR(32)	                NOT NULL
+);
+
+INSERT INTO foodtypes(id,type_name) VALUES (DEFAULT, 'standalone');
+INSERT INTO foodtypes(id,type_name) VALUES (DEFAULT, 'ingredient');
+INSERT INTO foodtypes(id,type_name) VALUES (DEFAULT, 'condiment');
+
+-- table of locations
+-- optional details help user clarify location
+CREATE TABLE locations (
+    id	            SERIAL          PRIMARY KEY                 NOT NULL,
+    location_name   VARCHAR(32)                                 NOT NULL,
+    details         VARCHAR(160),
+    date_added      DATE                                        NOT NULL,
+    added_by        INT             REFERENCES users(id)        NOT NULL,
+    date_modified   DATE                                        NOT NULL
+);
+
+-- will be a pre-seeded table of units
+-- servings, cups, ounces, etc.
+-- specifies unit (name or abbreviation) for user to assign food quantity
+CREATE TABLE units (
+    id          SERIAL      PRIMARY KEY     NOT NULL,
+    unit_name   VARCHAR(16)                 NOT NULL,
+    unit_abbr   VARCHAR(4),
+    UNIQUE(unit_name),
+    UNIQUE(unit_abbr)
+);
+
+-- table of foods
+-- user enters food item (name, quantity, etc.)
+CREATE TABLE foods (
+    id              SERIAL          PRIMARY KEY                 NOT NULL,
+    food_name       VARCHAR(32)                                 NOT NULL,
+    details         VARCHAR(160),
+    location_id     INT             REFERENCES locations(id)    NOT NULL,
+    foodtype_id     INT             REFERENCES foodtypes(id)    NOT NULL,
+    quantity        INT                                         NOT NULL,
+    units           INT             REFERENCES units(id),        
+    date_added      DATE                                        NOT NULL,
+    added_by        INT             REFERENCES users(id)        NOT NULL,
+    date_modified   DATE                                        NOT NULL
+);
+
+-- table of changes in quantity
+-- lists each instance where a food item's amount (and possibly units) is modified, with the before and after values
+CREATE TABLE changes_quantity (
+    id              SERIAL  PRIMARY KEY             NOT NULL,
+    food_id         INT     REFERENCES foods(id)    NOT NULL,
+    prev_amount     INT                             NOT NULL,
+    prev_units      INT     REFERENCES units(id),
+    new_amount      INT                             NOT NULL,
+    new_units       INT     REFERENCES units(id),
+    date_modified   DATE                            NOT NULL,
+    modified_by     INT     REFERENCES users(id)    NOT NULL
+);
+
+-- table of changes of food location
+-- lists each instance where a food item's location is modified
+CREATE TABLE changes_location (
+    id                  SERIAL  PRIMARY KEY                 NOT NULL,
+    food_id             INT     REFERENCES foods(id)        NOT NULL,
+    prev_location_id    INT     REFERENCES locations(id)    NOT NULL,
+    new_location_id     INT     REFERENCES locations(id)    NOT NULL,
+    date_modified       DATE                                NOT NULL,
+    modified_by         INT     REFERENCES users(id)        NOT NULL
+);
