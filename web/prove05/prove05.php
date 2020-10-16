b166063d3072ff09974af748050d11fc60039e3a
Travis Foutz <tfoutz@byui.edu>
2020-10-16 16:14:12 -0700

    testing/debugging

--- web/prove05/prove05.php
+++ web/prove05/prove05.php
@@ -62,10 +62,15 @@
                 $id = $_POST['locations'];
                 $statement = $db->prepare('SELECT food_name, quantity, unit FROM foods WHERE location_id = :id');
                 $statement->execute(array(':id' => $id));
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<tr><td>' . $row['food_name'] . '</td><td>' . $row['quantity'] . ' ' . $row['unit'];
-                    if ($row['quantity'] != 1 && $row['unit']) { echo 's';}
-                    echo '</td></tr>';
+                if ($statement) {
+                    echo '<table><tr><th>Food</th><th>Quantity</th></tr>';
+                    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                        echo '<tr><td>' . $row['food_name'] . '</td><td>' . $row['quantity'] . ' ' . $row['unit'];
+                        if ($row['quantity'] != 1 && $row['unit']) { echo 's';}
+                        echo '</td></tr>';
+                    }
+                } else {
+                    echo 'No food at this location.'
                 }
                 echo '</table>';
             }
