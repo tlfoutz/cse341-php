48a2afd547a0b4e576585a756552cb91ec2ecf96
Travis Foutz <tfoutz@byui.edu>
2020-10-21 12:21:42 -0700

    testing/debugging w06ta

--- web/teamActivities/ta06/topicEntry.php
+++ web/teamActivities/ta06/topicEntry.php
@@ -41,7 +41,7 @@ $db = get_db();
 
 <h1>Enter New Scriptures and Topics</h1>
 
-<form id="mainForm" action="insertTopic.php" method="POST">
+<form id="mainForm" method="POST">
 
 	<input type="text" id="txtBook" name="txtBook"></input>
 	<label for="txtBooK">Book</label>
