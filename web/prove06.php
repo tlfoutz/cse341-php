1b7aa26c9d7b83a5a3f15557f8d33fb02d641465
Travis Foutz <tfoutz@byui.edu>
2020-10-22 16:33:03 -0700

    testing/debugging prove06

--- web/prove06.php
+++ web/prove06.php
@@ -119,10 +119,10 @@
                 echo '<h3>Add new food location:</h3><label for="lAddName">Name:</label><br><input type="text" id="lAddName" name="lAddName"><br>';
                 echo '<label for="lAddDetails">Details:</label><br><input type="textarea" id="lAddDetails" name="lAddDetails"><br><br>';
                 
-                if ($_POST['lAddName']) {
-                    $statement = $db->prepare('INSERT INTO locations(location_name, data_added, added_by, date_modified) VALUES (:name, CURRENT_DATE, :id, CURRENT_DATE)');
-                    $statement->execute(array(':name' => $_POST['lAddName'], ':id' => $_SESSION['userId']));
-                }
+                // if ($_POST['lAddName']) {
+                //     $statement = $db->prepare('INSERT INTO locations(location_name, data_added, added_by, date_modified) VALUES (:name, CURRENT_DATE, :id, CURRENT_DATE)');
+                //     $statement->execute(array(':name' => $_POST['lAddName'], ':id' => $_SESSION['userId']));
+                // }
             }
 
             echo '<input type="submit" name="submit" value="Submit"></form><br>';
