66acdab44dbd5e7cdf1d5b0d6d9ce1adad9f08a7
Travis Foutz <tfoutz@byui.edu>
2020-10-23 11:55:25 -0700

    testing/debugging prove06

--- web/prove06.php
+++ web/prove06.php
@@ -43,8 +43,8 @@
                 $statement = $db->prepare('INSERT INTO foods(food_name, details, location_id, quantity, unit, added_by) VALUES (:name, :details, :locationId, :amount, :units, :id)');
                 $statement->execute(array(':name' => htmlspecialchars($_POST['fAddName']), ':details' => htmlspecialchars($_POST['fAddDetails']), ':locationId' => $_POST['fAddLocation'], ':amount' => $_POST['fAddQuantity'], ':units' => $_POST['fAddUnits'], ':id' => $_SESSION['userId'])); 
             } else if ($_POST['fAddUnits']) {
-                $statement = $db->prepare('INSERT INTO foods(food_name, location_id, quantity, units, added_by) VALUES (:name, :locationId, :amount, :units, :id)');
-                $statement->execute(array(':name' => htmlspecialchars($_POST['fAddName']), ':locationId' => $_POST['fAddLocation'], ':amount' => $_POST['fAddQuantity'], ':units' => $_POST['fAddUnits'], ':id' => $_SESSION['userId']));  
+                $statement = $db->prepare('INSERT INTO foods(food_name, location_id, quantity, added_by, unit) VALUES (:name, :details, :locationId, :amount, :id, :measurement)');
+                $statement->execute(array(':name' => htmlspecialchars($_POST['fAddName']), ':locationId' => $_POST['fAddLocation'], ':amount' => $_POST['fAddQuantity'], ':id' => $_SESSION['userId'], ':measurement' => $_POST['fAddUnits']));  
             } else if ($_POST['fAddDetails']) {
                 $statement = $db->prepare('INSERT INTO foods(food_name, details, location_id, quantity, added_by) VALUES (:name, :details, :locationId, :amount, :id)');
                 $statement->execute(array(':name' => htmlspecialchars($_POST['fAddName']), ':details' => htmlspecialchars($_POST['fAddDetails']), ':locationId' => $_POST['fAddLocation'], ':amount' => $_POST['fAddQuantity'], ':id' => $_SESSION['userId'])); 
