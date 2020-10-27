3a36577855332b90fa1f9362aec859205973ca95
Travis Foutz <tfoutz@byui.edu>
2020-10-27 13:07:19 -0700

    testing/debugging project01

--- web/project01/index.php
+++ web/project01/index.php
@@ -144,7 +144,7 @@
                 if ($_SESSION['selectedLocation'] == 0 || empty($_SESSION['selectedLocation'])) {
                     if ($_SESSION['foodSearch']) {
                         // TODO:
-                        $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE "pattern" AND f.added_by = :id ORDER BY f.food_name');
+                        $statement = $db->prepare("SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE 'pattern' AND f.added_by = :id ORDER BY f.food_name");
                         $statement->execute(array(':pattern' => '%' . lower($_SESSION['foodSearch']) . '%'));
                     } else {
                         $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE f.added_by = :id ORDER BY f.food_name');
@@ -157,7 +157,7 @@
                 } else {
                     if ($_SESSION['foodSearch']) {
                         // TOD0:
-                        $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE "pattern" AND f.added_by = :id AND f.location_id = :locationId ORDER BY f.food_name');
+                        $statement = $db->prepare("SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE 'pattern' AND f.added_by = :id AND f.location_id = :locationId ORDER BY f.food_name");
                         $statement->execute(array(':pattern' => '%' . lower($_SESSION['foodSearch']) . '%'));
                     } else {
                         $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE f.added_by = :id AND f.location_id = :locationId ORDER BY f.food_name');
