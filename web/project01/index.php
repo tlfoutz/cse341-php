8ea8a556b2efd7dd83f0e8f8298466f65cedfe85
Travis Foutz <tfoutz@byui.edu>
2020-10-27 14:16:53 -0700

    testing/debugging project01

--- web/project01/index.php
+++ web/project01/index.php
@@ -138,24 +138,32 @@
                 echo '<h2>Welcome, ' . $_SESSION['userName'] . '</h2>';
                 
                 // Food table
-                echo '<table><tr><th>Food</th><th>Location</th><th>Quantity</th><th>Details</th></tr>';
+                echo '<table><tr><th>Food</th><th>Location</th><th>Quantity</th></tr>';
                 $counter = 0;
                 
                 if ($_SESSION['selectedLocation'] == 0 || empty($_SESSION['selectedLocation'])) {
                     if ($_SESSION['foodSearch']) {
-                        $statement = $db->prepare("SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE :pattern AND f.added_by = :id ORDER BY f.food_name");
+                        $statement = $db->prepare("SELECT f.id, f.food_name, f.location_id, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE :pattern AND f.added_by = :id ORDER BY f.food_name");
                         $statement->execute(array(':id' => $_SESSION['userId'], ':pattern' => '%' . $_SESSION['foodSearch'] . '%'));
                     } else {
-                        $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE f.added_by = :id ORDER BY f.food_name');
+                        $statement = $db->prepare('SELECT f.id, f.food_name, f.location_id, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE f.added_by = :id ORDER BY f.food_name');
                         $statement->execute(array(':id' => $_SESSION['userId']));
                     }
                     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<tr><td>' . $row['food_name'] . '</td><td>' . $row['location_name'] . '</td><td><input type="number" value="' . $row['quantity'] . '" name="newAmount' .$row['id'] . '" min="0"></td><td>' . $row['details'] . '</td></tr>';
+                        echo '<tr><td>' . $row['food_name'] . '</td><td>';
+                        echo '<select name="' . $row['id'] . 'locations" id="' . $row['id'] . 'locations"><option value="0" disabled> -- Select location -- </option>';
+                        $statement = $db->prepare('SELECT id, location_name FROM locations WHERE added_by = :id ORDER BY location_name');
+                        $statement->execute(array(':id' => $_SESSION['userId']));
+                        while ($innerRow = $statement->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="' . $innerRow['id'] . '"';
+                            if ($row['location_id'] == $innerRow['id']) { echo ' selected'; }
+                            echo '>' . $innerRow['location_name'] . '</option>';
+                        }
+                        echo '</select></td><td><input type="number" value="' . $row['quantity'] . '" name="newAmount' .$row['id'] . '" min="0"></td></tr>';
                         $counter++;
                     }
                 } else {
                     if ($_SESSION['foodSearch']) {
-                        // TOD0:
                         $statement = $db->prepare("SELECT f.id, f.food_name, f.location_id, f.details, f.quantity, l.location_name FROM foods f INNER JOIN locations l ON f.location_id = l.id WHERE lower(f.food_name) LIKE :pattern AND f.added_by = :id AND f.location_id = :locationId ORDER BY f.food_name");
                         $statement->execute(array(':id' => $_SESSION['userId'], ':locationId' => $_SESSION['selectedLocation'], ':pattern' => '%' . $_SESSION['foodSearch'] . '%'));
                     } else {
@@ -184,7 +192,7 @@
                 
                 // Food name search 
                 echo '<label for="fname">Find food by name:</label><br><input type="search" id="fname" name="fname"';
-                if($_SESSION['foodSearch']) { echo ' value="' . $_SESSION['foodSearch'] . '"';}
+                if($_SESSION['foodSearch']) { echo ' value="' . $_POST['fname'] . '"';}
                 echo '><br>';
 
                 // New food item input
