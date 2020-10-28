a2fbd634f285f4ca19b0491ff3e2d789c95013d7
Travis Foutz <tfoutz@byui.edu>
2020-10-28 14:52:20 -0700

    testing/debugging project01

--- web/project01/setUser.php
+++ web/project01/setUser.php
@@ -3,7 +3,12 @@
     require "dbConnect.php";
 
     $statement = $db->prepare('SELECT id, user_name, user_password FROM users WHERE user_name = :username AND user_password = :password');
-    $statement->execute(array(':username' => htmlspecialchars($_POST['rname']), ':password' => htmlspecialchars($_POST['rpsw'])));
+    try{$statement->execute(array(':username' => htmlspecialchars($_POST['rname']), ':password' => htmlspecialchars($_POST['rpsw'])));}
+    catch (PDOException $ex) {
+        $_SESSION['errMsg'] = '<p style="color:red">Incorrect username/password.</p>';
+        header("Location: signIn.php");
+        die();
+    }
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         if ($row['user_name'] == $_POST['rname'] && $row['user_password'] == $_POST['rpsw']) {
             $_SESSION['userId'] = $row['id'];
