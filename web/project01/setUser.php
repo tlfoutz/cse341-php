fb0d9f8f0cf3e488aa9a73e9f950a9e7a5b8c626
Travis Foutz <tfoutz@byui.edu>
2020-10-28 14:48:21 -0700

    testing/debugging project01

--- web/project01/setUser.php
+++ web/project01/setUser.php
@@ -11,15 +11,15 @@
             $_SESSION['errMsg'] = '';
             header("Location: index.php");
             die();
-        } else if ($row['user_name'] != $_POST['rname'] && $row['user_password'] != $_POST['rpsw']) {
+        } else if ($row['user_name'] !== $_POST['rname'] && $row['user_password'] !== $_POST['rpsw']) {
             $_SESSION['errMsg'] = '<p style="color:red">Incorrect username and password.</p>';
             header("Location: signIn.php");
             die();
-        } else if ($row['user_name'] != $_POST['rname']) {
+        } else if ($row['user_name'] !== $_POST['rname']) {
             $_SESSION['errMsg'] = '<p style="color:red">Incorrect username.</p>';
             header("Location: signIn.php");
             die();
-        } else if ($row['user_password'] == $_POST['rpsw']) {
+        } else if ($row['user_password'] !== $_POST['rpsw']) {
             $_SESSION['errMsg'] = '<p style="color:red">Incorrect username.</p>';
             header("Location: signIn.php");
             die();
