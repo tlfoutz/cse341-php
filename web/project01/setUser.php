976d94243a506abf6fa9ed3341c604171fae0bca
Travis Foutz <tfoutz@byui.edu>
2020-10-28 14:45:44 -0700

    testing/debugging project01

--- web/project01/setUser.php
+++ web/project01/setUser.php
@@ -2,45 +2,29 @@
     session_start();
     require "dbConnect.php";
 
-	$username = $_POST['rname'];
-	$password = $_POST['rpsw'];
-
     $statement = $db->prepare('SELECT id, user_name, user_password FROM users WHERE user_name = :username AND user_password = :password');
-    $result = $statement->execute(array(':username' => htmlspecialchars($_POST['rname']), ':password' => htmlspecialchars($_POST['rpsw'])));
-
-	if ($result)
-	{
-		$row = $statement->fetch();
-		$hashedPasswordFromDB = $row['password'];
-
-		if (password_verify($password, $hashedPasswordFromDB))
-		{
+    $statement->execute(array(':username' => htmlspecialchars($_POST['rname']), ':password' => htmlspecialchars($_POST['rpsw'])));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        if ($row['user_name'] == $_POST['rname'] && $row['user_password'] == $_POST['rpsw']) {
             $_SESSION['userId'] = $row['id'];
             $_SESSION['userName'] = $row['user_name'];
+            $_SESSION['errMsg'] = '';
             header("Location: index.php");
-			die();
-		} else {
-            $_SESSION['errMsg'] = '<p style="color:red">Incorrect password.</p>';
+            die();
+        } else if ($row['user_name'] != $_POST['rname'] && $row['user_password'] != $_POST['rpsw']) {
+            $_SESSION['errMsg'] = '<p style="color:red">Incorrect username and password.</p>';
+            header("Location: signIn.php");
+            die();
+        } else if ($row['user_name'] != $_POST['rname']) {
+            $_SESSION['errMsg'] = '<p style="color:red">Incorrect username.</p>';
+            header("Location: signIn.php");
+            die();
+        } else if ($row['user_password'] == $_POST['rpsw']) {
+            $_SESSION['errMsg'] = '<p style="color:red">Incorrect username.</p>';
             header("Location: signIn.php");
             die();
         }
-	} else {
-        $_SESSION['errMsg'] = '<p style="color:red">Incorrect username.</p>';
-        header("Location: signIn.php");
-        die();
     }
 
-    $statement = $db->prepare('SELECT id, user_name, user_password FROM users WHERE user_name = :username AND user_password = :password');
-    try {$statement->execute(array(':username' => htmlspecialchars($_POST['rname']), ':password' => htmlspecialchars($_POST['rpsw'])));}
-    catch (PDOException $ex) {
-
-    }
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $_SESSION['userId'] = $row['id'];
-        $_SESSION['userName'] = $row['user_name'];
-    }
-    $_SESSION['errMsg'] = '';
-    header("Location: index.php");
-    die();
 
 ?>
