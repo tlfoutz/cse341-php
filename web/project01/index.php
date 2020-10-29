61a95c00dd9b995fcd110f05251501ad431c0ab5
Travis Foutz <tfoutz@byui.edu>
2020-10-29 14:57:09 -0700

    testing/debugging project01

--- web/project01/index.php
+++ web/project01/index.php
@@ -63,7 +63,7 @@
         </header>
         <div class="container features">
           <div class="row">
-            <div class="col-lg-1">
+            <div class="col">
             <table><tr><th>Food</th><th>Location</th><th>Details (Location)</th><th>Quantity</th><th>Details (Food)</th></tr>
                         <?php include 'renderFoodTable.php'; ?>
                     </table><br>
